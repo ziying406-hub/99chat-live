7407d77625bbcd73a809a8826b276d51c78dabae
ziying406-hub <298246065+ziying406-hub@users.noreply.github.com>
2026-07-13 18:40:21 +0800

    Version avatar profile interaction

--- apps/web/src/app.js
+++ apps/web/src/app.js
@@ -59,7 +59,7 @@ import { uploadErrorMessage, validateSignedUpload } from "./uploadErrors.js";
 
 const API_BASE = resolveApiBase();
 const WS_BASE = resolveWebSocketBase(API_BASE);
-const APP_VERSION = "20260713-realtime-sync-stable";
+const APP_VERSION = "20260713-message-avatar-profile";
 const APP_VERSION_KEY = "chatlite-app-version";
 const MOCK_GROUP_NICKNAMES_KEY = "chatlite-mock-group-nicknames";
 const MOCK_GROUP_TITLES_KEY = "chatlite-mock-group-titles";
